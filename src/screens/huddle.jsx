3636c45335c9eb1de1040f131a185d8b28bd5620
0xdev <zinzuvadiyameet98@gmail.com>
2024-01-09 14:38:45 +0530

    Update service from scratch, ui code commited

--- src/screens/huddle.jsx
+++ src/screens/huddle.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./navbar";
-import { sessionData } from "../services/huddleServices";
-import { AccessToken, Role } from "@huddle01/server-sdk/auth";
-import { useRoom } from '@huddle01/react/hooks';
+import axios from "axios";
+
 
 export default function Huddle() {
 
@@ -10,16 +9,6 @@ export default function Huddle() {
     const partnerVideo = React.useRef();
     const [roomId, setRoomId] = React.useState("");
     const [accessToken, setAccessToken] = React.useState();
-
-    const { joinRoom, leaveRoom } = useRoom({
-        onJoin: () => {
-            console.log('Joined the room');
-        },
-        onLeave: () => {
-            console.log('Left the room');
-        },
-    });
-
 
     useEffect(() => {
         navigator.mediaDevices
@@ -30,27 +19,10 @@ export default function Huddle() {
             }).catch(error => {
                 console.log("Error: ", error);
             });
-        setRoomId(sessionData.data.data.roomId);
-        generateToken();
     }, []);
 
 
-
-
-    const generateToken = async () => {
-
-        const accessToken = new AccessToken({
-            apiKey: process.env.HUDDLE_API_KEY,
-            roomId: roomId,
-            role: Role.HOST,
-        });
-
-        setAccessToken(accessToken);
-    }
-
-
     return (
-
         <div className="relative h-screen">
             <div className="absolute inset-x-0 top-0">
                 <Navbar />
@@ -58,20 +30,12 @@ export default function Huddle() {
             <div className="absolute inset-0">
                 <div className="flex items-center justify-center gap-4 h-full">
                     <video className="w-96 h-72 shadow-lg rounded-2xl saturate-150" ref={userVideo} autoPlay muted playsInline />
-                    <video className="w-96 h-72 bg-slate-200 shadow-lg rounded-2xl saturate-150" ref={partnerVideo} autoPlay muted playsInline />
+                    <video className="w-96 h-72  shadow-lg rounded-2xl saturate-150" ref={userVideo} autoPlay muted playsInline />
                 </div>
             </div>
             <div className="absolute inset-x-0 bottom-0 w-screen text-slate-400 p-5 text-center">
                 <h1>@ Develop In BitsBrewLab with ❤️</h1>
-                <button onClick={() => {
-                    joinRoom({
-                        roomId: roomId,
-                        token: accessToken
-                    });
-                }}>Join Room</button>
             </div>
         </div>
-
-
     );
 }
